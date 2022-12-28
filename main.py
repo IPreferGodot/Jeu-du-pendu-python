d1ddf0dfea4b77fb1c4c48c08be36c26d9e5a3b3
IPreferGodot <marius.bardel@csilyon.fr>
2022-12-28 19:51:57 +0100

    larousse-api et multiprocessing
    Pfiou, comme larousse-api prend un temps fou à trouver les définitions, j'ai été obligé de créer un processus parallèle...

--- main.py
+++ main.py
@@ -1,70 +1,90 @@
 from path_rectifier import *
-import pygame, sys, time
-from word_chooser import choose_word
+import pygame, sys, time, word_chooser
+from word_chooser import get_word_async as get_word
 from pygame.locals import QUIT
 
-pygame.init()
+if __name__ == "__main__":
+    print("initializing pygame...")
+    pygame.init()
+    print("initializing word_chooser...")
+    word_chooser.init_process()
 
-# ----------------- Constants ------------------
-FRAME_TIME = 1/2
-BACKGROUND_COLOR = 70,  70,  70
+    # ----------------- Constants ------------------
+    FRAME_TIME = 1/2
+    BACKGROUND_COLOR = 70,  70,  70
 
-DISPLAYSURF = pygame.display.set_mode((400, 400))
-pygame.display.set_caption("Hangman")
+    DISPLAYSURF = pygame.display.set_mode((400, 400))
+    pygame.display.set_caption("Hangman")
 
-# ------------------- Events -------------------
-MUSIC_END = pygame.USEREVENT + 1
+    # ------------------- Events -------------------
+    MUSIC_END = pygame.USEREVENT + 1
 
-# --------------- XenozK Version ---------------
+    # ----------------- Variables ------------------
+    prechoosed_words: list[str] = []
 
-# la taille de la page et le titre
-#window_size = (400, 400)
-#window_title = "Hangman"
-#screen = pygame.display.set_mode(window_size)
-#pygame.display.set_caption(window_title)
+    # --------------- XenozK Version ---------------
 
-# couleur du background
-bg_color = (255, 255, 255)
+    # la taille de la page et le titre
+    #window_size = (400, 400)
+    #window_title = "Hangman"
+    #screen = pygame.display.set_mode(window_size)
+    #pygame.display.set_caption(window_title)
 
-# couleur du text
-font = pygame.font.Font(None, 32)
-text_color = (0, 0, 0)
+    # couleur du background
+    bg_color = (255, 255, 255)
 
-
-hangman_largeur = 200
-hangman_longeur = 300
-
-#prend les images
-hangman_images = []
-for i in range(4):
-    image = pygame.image.load(resource_path(f"assets/img/sprites/hangman/hangman{i}.png"))
-    image = pygame.transform.scale(image, (hangman_largeur, hangman_longeur))
-    hangman_images.append(image)
+    # couleur du text
+    font = pygame.font.Font(None, 32)
+    text_color = (0, 0, 0)
 
 
-pygame.mixer.music.load(resource_path(r"assets/music/Level 1.ogg"))
-pygame.mixer.music.set_endevent(MUSIC_END)
-pygame.mixer.music.play()
-pygame.mixer.music.queue(resource_path(r"assets/music/Transition 1-2.ogg"))
-pygame.mixer.music.set_volume(0.5)
+    hangman_largeur = 200
+    hangman_longeur = 300
 
-test = 0
-next_frame = time.time() - 1
-while True:
-    if time.time() > next_frame:
-        next_frame = time.time() + FRAME_TIME
-        print(choose_word(1000))
-        DISPLAYSURF.fill(BACKGROUND_COLOR)
-        test += 1
-        test %= 4
-        DISPLAYSURF.blit(hangman_images[test], hangman_images[0].get_rect())
+    #prend les images
+    hangman_images = []
+    for i in range(4):
+        image = pygame.image.load(resource_path(f"assets/img/sprites/hangman/hangman{i}.png"))
+        image = pygame.transform.scale(image, (hangman_largeur, hangman_longeur))
+        hangman_images.append(image)
 
-    for event in pygame.event.get():
-        what = event.type
-        if what == QUIT:
-            pygame.quit()
-            sys.exit()
-        elif what == MUSIC_END:
-            pygame.mixer.music.queue(resource_path(r"assets/music/Level 2.ogg"))
 
-    pygame.display.update()
+    pygame.mixer.music.load(resource_path(r"assets/music/Level 1.ogg"))
+    pygame.mixer.music.set_endevent(MUSIC_END)
+    pygame.mixer.music.play()
+    pygame.mixer.music.queue(resource_path(r"assets/music/Transition 1-2.ogg"))
+    pygame.mixer.music.set_volume(0.5)
+
+    test = 0
+    next_frame = time.time() - 1
+
+    while True:
+        if time.time() > next_frame:
+            next_frame = time.time() + FRAME_TIME
+            DISPLAYSURF.fill(BACKGROUND_COLOR)
+            test += 1
+            test %= 4
+            DISPLAYSURF.blit(hangman_images[test], hangman_images[0].get_rect())
+            if test == 1:
+                word_chooser.get_word_async(1000)
+                print("prechoosed_words lenght :", len(prechoosed_words), "| word :", prechoosed_words.pop() if len(prechoosed_words)!=0 else "EMPTY")
+
+        # if word_chooser.word_queue:
+        #     while not word_chooser.word_queue.empty():
+        #         prechoosed_words.append(word_chooser.word_queue.get())
+
+        if word_chooser.connection_parent:
+            while word_chooser.connection_parent.poll():
+                prechoosed_words.append(word_chooser.connection_parent.recv())
+
+
+        for event in pygame.event.get():
+            what = event.type
+            if what == QUIT:
+                word_chooser.terminate()
+                pygame.quit()
+                sys.exit()
+            elif what == MUSIC_END:
+                pygame.mixer.music.queue(resource_path(r"assets/music/Level 2.ogg"))
+
+        pygame.display.update()
--- word_chooser.py
+++ word_chooser.py
@@ -1,9 +1,28 @@
 DEBUG = False
 
+
 print("Importing...")
 import pandas
 from path_rectifier import *
 
+
+HAS_LAROUSSE = False
+try:
+    from larousse_api import larousse
+    HAS_LAROUSSE = True
+except:
+    print("Le module larousse-api n'est pas installé")
+
+
+connection_parent: "None|multiprocessing.connection.Connection" = None
+_connection_child: "None|multiprocessing.connection.Connection" = None
+process: "None|multiprocessing.Process" = None
+
+if HAS_LAROUSSE:
+    import multiprocessing, multiprocessing.connection # (import connection just for type hint)
+
+
+print("Loading words...")
 words: pandas.DataFrame = pandas.read_csv(resource_path(r"assets/data/words.csv"))
 print("Loaded words.")
 
@@ -12,13 +31,40 @@ UP_RANGE_MIN: int = 500  # Valeur minimale du bas de la plage aléatoire
 WORDS_COUNT = len(words)
 RANGE = 1000
 
+DEFAULT_MAX_ATTEMPTS = 10
+
+class Word():
+    """Store a word, with some informations about it."""
+    def __init__(self, word: str, difficulty: int, definitions: list[str]|None = None) -> None:
+        self.word: str = word
+        self.difficulty: int = difficulty
+
+        self.has_definitions: bool = False
+        self.definitions: list[str] = ["This word has no definition."]
+
+        if definitions:
+            self.has_definitions = True
+            self.definitions = definitions
+
+    def __str__(self) -> str:
+        return self.word
+
+    def __repr__(self) -> str:
+        return f"{self.word} ({self.difficulty})"
+
+
+class WordRequest():
+    def __init__(self, difficulty: int, max_attempts: int = DEFAULT_MAX_ATTEMPTS) -> None:
+        self.difficulty: int = difficulty
+        self.max_attempts: int = max_attempts
+
 
 def cap(value: int, minimum: int, maximum: int) -> int:
     """Cappe la valeur entre les bornes `minimum` et `maximum`"""
     return min(max(value, minimum), maximum)
 
 
-def choose_word(difficulty: int) -> str:
+def get_word(difficulty: int, max_attempts: int = DEFAULT_MAX_ATTEMPTS) -> str:
     """Choisit aléatoirement un mot dont l'indice est est situé entre `difficulty - word_chooser.RANGE` et `difficulty + word_chooser.RANGE`"""
 
     down_range = cap(difficulty - RANGE, 0, DOWN_RANGE_MAX)
@@ -27,10 +73,96 @@ def choose_word(difficulty: int) -> str:
     if DEBUG:
         print(difficulty, " → ", down_range, "-", up_range, sep="")
 
-    return words.iloc[down_range:up_range].sample(1).iloc[0]["word"]
+    attempts = 0
+    while attempts < max_attempts:
+        word = words.iloc[down_range:up_range].sample(1).iloc[0]["word"]
 
+        print("attempt")
+
+        if HAS_LAROUSSE:
+            definitions = larousse.get_definitions(word)
+            if definitions:
+                return Word(word, difficulty, definitions)
+        else:
+            return Word(word, difficulty)
+
+        print("[W] This word is not in the larousse dictionnary :", word)
+        attempts += 1
+
+    return "inexistant"
+
+
+def process_loop(connection: "multiprocessing.connection.Connection") -> None:
+    """Start the subprocess infinite loop."""
+
+    print("i am here")
+    while True:
+        print("Loop start")
+        connection.poll(None) # Wait for a message
+        print("message !")
+        message = connection.recv()
+
+        if message == "close":
+            return
+        elif isinstance(message, WordRequest):
+            connection.send(get_word(message.difficulty, message.max_attempts))
+        else:
+            print("[W] Unknow message passed trough pipe :", message)
+
+
+def get_word_async(difficulty: int, max_attempts: int = DEFAULT_MAX_ATTEMPTS) -> str:
+    """Si larousse-api est installé, demande au programme parallèle de chercher un mot sur le larousse."""
+
+    if not HAS_LAROUSSE:
+        return Exception("larousse-api is not installed, use `get_word()` instead")
+    if not connection_parent:
+        return Exception("Initialisez d'abord le processus parallèle avec `init_process()`")
+
+    connection_parent.send(WordRequest(difficulty, max_attempts))
+
+
+def terminate() -> None:
+    if not process:
+        print("[W] `terminate()` is unnecessary as no process has been created.")
+        return
+
+    process.terminate()
+    process.join()
+    connection_parent.close()
+    _connection_child.close()
+
+
+def init_process() -> None:
+    """Call this to create the new process. This prevent the new process to create a process wich would create a process wich would etc..."""
+
+
+
+    global process, connection_parent, _connection_child
+
+    print("initializing process")
+
+    if not HAS_LAROUSSE:
+        return Exception("Starting a process is not necessary : larousse-api is not installed.")
+    if process:
+        return Exception("Process was already created.")
+
+    connection_parent, _connection_child = multiprocessing.Pipe(duplex=True)
+    process = multiprocessing.Process(target=process_loop, args=(_connection_child,), )
+    process.start()
 
 
 if True and __name__ == "__main__":
-    for i in range(-1000, len(words) + 1001, 1000):
-        print(i, ":", choose_word(i))
+    # for i in range(-1000, len(words) + 1001, 1000):
+    #     print(i, ":", get_word(i))
+
+    # get_word_async(0)
+    # print("i'am first")
+
+    # while word_queue.empty():
+    #     print("empty")
+
+    # print(word_queue.get())
+    # word_queue.close()
+
+    if HAS_LAROUSSE:
+        init_process()
